602fc48bfc9c2220d2da7f2ec5e85197f5e278ba
navidsarvian <navidsarv@gmail.com>
2018-01-10 21:10:17 -0800

    updateagain

--- assets/employee.js
+++ assets/employee.js
@@ -1,6 +1,5 @@
 var trow = $("<tr>");
 var tcell = $("<td>");
-
 //Capture the value of user input
 //Append those values each tcell
 //Append each tcell to the trow
@@ -12,28 +11,22 @@ var config = {
     databaseURL: "https://employee-rate-project.firebaseio.com",
     projectId: "employee-rate-project",
 };
-
 firebase.initializeApp(config);
-
 // Create a variable to reference the database.
 var database = firebase.database();
-
 // Initial Values
 var name = "";
 var role = "";
 var date = 0;
 var pay = "";
-
 // Capture Button Click
 $("#add-user").on("click", function(event) {
   event.preventDefault();
-
   // Grabbed values from text boxes
   name = $("#example-text-input").val().trim();
   role = $("#example-role-input").val().trim();
   date = $("#example-datetime-local-input").val().trim();
   pay = $("#example-number-input").val().trim();
-
   // Code for handling the push
   database.ref().push({
     name: name,
@@ -42,19 +35,15 @@ $("#add-user").on("click", function(event) {
     pay: pay,
     dateAdded: firebase.database.ServerValue.TIMESTAMP
   });
-
-
 // Firebase watcher + initial loader + order/limit HINT: .on("child_added"
 database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
   // storing the snapshot.val() in a variable for convenience
   var sv = snapshot.val();
-
   // Console.loging the last user's data
   console.log(sv.name);
   console.log(sv.role);
   console.log(sv.date);
   console.log(sv.pay);
-
   // Change the HTML to reflect
 /*  $("#name-display").text(sv.name);
   $("#email-display").text(sv.role);
@@ -65,4 +54,12 @@ database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", functi
 }, function(errorObject) {
   console.log("Errors handled: " + errorObject.code);
 });
+  database.ref().on('child_added', function(snapshot){
+    $('#name').text(snapshot.val().name);
+    $('#role').text(snapshot.val().role);
+    $('#start-date').text(snapshot.val().role);
+    $('#monthly-rate').text(snapshot.val().role);
+}, function(errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+})
 });
